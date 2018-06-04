17cf147bc34f998b2ff71c41e5c5285072a7e759
Omar <omar21621@gmail.com>
2018-06-04 07:25:26 +0200

    first commit

---
+++ resources/views/admin/post/create.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+
+
+@section('content')
+
+    <div class="card">
+        <div class="card-header">
+            Create Post Form
+        </div>
+        <div class="card-body">
+            <form action="{{route('post.store')}}}" method="post" enctype="multipart/form-data">
+                {{csrf_field()}}
+                <div class="form-group">
+                    <label for="title">Title</label>
+                    <input type="text" name="title" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="image">Image</label>
+                    <input type="file" name="image" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="content">Content</label>
+                    <textarea type="text" name="content" class="form-control"></textarea>
+                </div>
+                <div class="form-group">
+                    <div class="text-center">
+                        <input type="submit" class="btn btn-primary" value="Create Post">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+
+@stop
